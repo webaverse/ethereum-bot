18c0da8c422172a8f1ff351c99423e8d632f418b
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-16 16:32:00 -0600

    add realm command

--- index.js
+++ index.js
@@ -524,7 +524,8 @@ Info
 .address [@user]? - print address
 .key - private key in DM
 .login - login link in DM
-.play [num] - play link in DM, optional party id (1-5)
+.play - play link in DM
+.realm [num] - play link in DM to realm id (1-5)
 
 Tokens
 .send [@user|0xaddr|treasury] [amount] - send FT
@@ -2265,9 +2266,9 @@ Keys (DM bot)
             }).promise();
             
             const m = await message.author.send(`Login: https://webaverse.com/login?id=${id}&code=${code}`);
-          } else if (split[0] === prefix + 'play') {
+          } else if (split[0] === prefix + 'realm') {
             const id = message.author.id;
-            let roomId = parseInt(split[1], 10);
+            let realmId = parseInt(split[1], 10);
 
             const code = new Uint32Array(crypto.randomBytes(4).buffer, 0, 1).toString(10).slice(-6);
             await ddb.putItem({
@@ -2278,15 +2279,29 @@ Keys (DM bot)
               }
             }).promise();
 
-            if (isNaN(roomId)) {
-              const m = await message.author.send(`Login: https://webaverse.com/login?id=${id}&code=${code}&play=true`);
+            if (isNaN(realmId)) {
+              message.channel.send('<@!' + message.author.id + '>: must add realm id. (1-5)');
             } else {
-              if (roomId >= 1 && roomId <= 5) {
-                const m = await message.author.send(`Login: https://webaverse.com/login?id=${id}&code=${code}&play=true&roomId=${roomId}`);
+              if (realmId >= 1 && realmId <= 5) {
+                const m = await message.author.send(`Play: https://webaverse.com/login?id=${id}&code=${code}&play=true&realmId=${realmId}`);
               } else {
-                 message.channel.send('<@!' + message.author.id + '>: party id must be between 1-5.');
+                 message.channel.send('<@!' + message.author.id + '>: realm id must be between 1-5.');
               }
             }
+
+          } else if (split[0] === prefix + 'play') {
+            const id = message.author.id;
+
+            const code = new Uint32Array(crypto.randomBytes(4).buffer, 0, 1).toString(10).slice(-6);
+            await ddb.putItem({
+              TableName: usersTableName,
+              Item: {
+                email: {S: id + '.code'},
+                code: {S: code},
+              }
+            }).promise();
+
+            const m = await message.author.send(`Login: https://webaverse.com/login?id=${id}&code=${code}&play=true`);
           } else if (split[0] === prefix + 'key') {
             let {mnemonic} = await _getUser();
             if (!mnemonic) {
