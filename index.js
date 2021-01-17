ba3ffda315f7291ef6e2451003c3fe325bf28a7e
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-01-17 17:54:07 -0600

    set name on login cmd

--- index.js
+++ index.js
@@ -2255,17 +2255,44 @@ Keys (DM bot)
             }
           } else if (split[0] === prefix + 'login') {
             const id = message.author.id;
-            
-            const code = new Uint32Array(crypto.randomBytes(4).buffer, 0, 1).toString(10).slice(-6);
-            await ddb.putItem({
-              TableName: usersTableName,
-              Item: {
-                email: {S: id + '.code'},
-                code: {S: code},
-              }
-            }).promise();
-            
-            const m = await message.author.send(`Login: https://webaverse.com/login?id=${id}&code=${code}`);
+
+            let {mnemonic} = await _getUser();
+            if (!mnemonic) {
+              const spec = await _genKey();
+              mnemonic = spec.mnemonic;
+            }
+            const wallet = hdkey.fromMasterSeed(bip39.mnemonicToSeedSync(mnemonic)).derivePath(`m/44'/60'/0'/0/0`).getWallet();
+            const address = wallet.getAddressString();
+
+            const currentName = await runSidechainTransaction(mnemonic)('Account', 'getMetadata', address, 'name');
+
+            if (currentName) {
+              const code = new Uint32Array(crypto.randomBytes(4).buffer, 0, 1).toString(10).slice(-6);
+              await ddb.putItem({
+                TableName: usersTableName,
+                Item: {
+                  email: {S: id + '.code'},
+                  code: {S: code},
+                }
+              }).promise();
+
+              const m = await message.author.send(`Login: https://webaverse.com/login?id=${id}&code=${code}`);
+            } else {
+              const discordName = Client.fetchUser(id);
+              const result = await runSidechainTransaction(mnemonic)('Account', 'setMetadata', address, 'name', name);
+
+              const code = new Uint32Array(crypto.randomBytes(4).buffer, 0, 1).toString(10).slice(-6);
+              await ddb.putItem({
+                TableName: usersTableName,
+                Item: {
+                  email: {S: id + '.code'},
+                  code: {S: code},
+                }
+              }).promise();
+
+              const m = await message.author.send(`Login: https://webaverse.com/login?id=${id}&code=${code}`);
+            }
+
           } else if (split[0] === prefix + 'realm') {
             const id = message.author.id;
             let realmId = parseInt(split[1], 10);
