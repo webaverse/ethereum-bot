f2b25eb9bb3777bc8ed5da2e8d355316032c2c4e
Avaer Kazmer <a@modules.io>
2021-01-20 06:22:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -156,9 +156,9 @@ const makePromise = () => {
   });
   const gethNodeUrl = `http://${ethereumHostAddress}`;
 
-  console.log('got web3', gethNodeUrl + ':' + (isMainnet ? '8545' : '8456'));
+  console.log('got web3', gethNodeUrl + ':' + (isMainnet ? '8545' : '8546'));
 
-  const web3 = new Web3(new Web3.providers.HttpProvider(gethNodeUrl + ':' + (isMainnet ? '8545' : '8456')));
+  const web3 = new Web3(new Web3.providers.HttpProvider(gethNodeUrl + ':' + (isMainnet ? '8545' : '8546')));
   web3.eth.transactionConfirmationBlocks = 1;
   const addresses = await fetch('https://contracts.webaverse.com/config/addresses.js').then(res => res.text()).then(s => JSON.parse(s.replace(/^\s*export\s*default\s*/, ''))[isMainnet ? 'mainnetsidechain' : 'rinkebysidechain']);
   const abis = await fetch('https://contracts.webaverse.com/config/abi.js').then(res => res.text()).then(s => JSON.parse(s.replace(/^\s*export\s*default\s*/, '')));
