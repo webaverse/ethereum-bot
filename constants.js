23e8f6ae0c9bfcdc8f50f09a47f26ea5b8cd5705
Avaer Kazmer <a@modules.io>
2021-09-03 03:34:58 -0400

    Revert "Merge pull request #21 from webaverse/add-roles"
    This reverts commit 4c3447fef6ad0f47f1d664224c01513557413ac6, reversing
    changes made to 125db4a3f7a5e27d1c5b55e4b0975724499e96ec.

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 exports.ethereumHost = 'ethereum.exokit.org';
-
+exports.usersTableName = 'users';
 exports.prefix = '.';
 exports.storageHost = 'https://ipfs.exokit.org';
 exports.previewHost = 'https://preview.exokit.org';
