8d759095c34e355e849a4388e3c6ce209607c086
Avaer Kazmer <a@modules.io>
2021-09-04 12:02:29 -0400

    Update discordbot.js

--- discordbot.js
+++ discordbot.js
@@ -2953,18 +2953,11 @@ document.body.appendChild(div);
 */
 
 /* Code */
-#version 330 core
-layout (location = 0) in vec3 aPos;
-  
-out vec4 vertexColor; // specify a color output to the fragment shader
-
-void main() {
-  gl_Position = vec4(aPos, 1.0); // see how we directly give a vec3 to vec4's constructor
-  vertexColor = vec4(0.5, 0.0, 0.0, 1.0); // set the output variable to a dark-red color
-}
+num = 12
+for i in range(1, 11):
+  print(num, 'x', i, '=', num*i)
 /* Explanation:
-- A vertex shader that draws red triangles.
-- You also need the fragment shader.
+- Outputs a multiplication table for the number 12.
 */
 
 /* Code */
