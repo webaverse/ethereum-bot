9c58fdd9231b19480254ef76a7aab1af44abe3ea
Empec <rob@raspberrydreamlabs.com>
2022-07-13 18:50:27 +0100

    changed fs and path requirements

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('node:fs');
+const path = require('node:path');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
